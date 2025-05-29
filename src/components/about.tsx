20e35473a48c9e0393e7888e45004c99083a1850
RUKA-BOT-TEST <rukabottest@gmail.com>
2025-05-29 20:50:14 +0530

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -46,7 +46,7 @@ export const About = () => {
         {/* Title */}
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>Introduction</p>
-          <h2 className={styles.sectionHeadText}>Overview.</h2>
+          <h2 className={styles.sectionHeadText}>Overview. (Project)</h2>
         </motion.div>
 
         {/* Body */}
